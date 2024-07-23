9ff42378c3d8a1a918ace4f43655fcb18370fa2b
Miguel Mercedes <lmyguel0324@gmail.com>
2024-07-23 13:09:29 -0400

    refactor: Update API endpoint in products.ts API handler

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,14 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { IProduct } from '../../interfaces/product.interface';
-import prisma from '../../prisma/prismaClient';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { IProduct } from '../../interfaces/product.interface'
+import prisma from '../../prisma/prismaClient'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<IProduct[]>) {
-
-  const products = await prisma.product.findMany({
-    where: {
-      categoryId: 1,
-    },
-  });
-
-  res.status(200).json(products as IProduct[]);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        categoryId: 1
+      }
+    })
+    res.status(200).json(products as IProduct[])
+  } catch (error: any) {
+    res.status(500).json({ error: 'An error occurred while fetching products' })
+  }
 }
